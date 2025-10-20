0459871b706105366826bdd60e1474347b9c30cf
Willington <aeronautica2001@gmail.com>
2025-10-19 19:29:13 -0500

    Agregar worker.py

---
+++ worker.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Worker: ejecuta el loop de recordatorios (iniciar_sistema).
+Start command en Render: python worker.py
+"""
+
+from estilista import iniciar_sistema
+
+if __name__ == "__main__":
+    iniciar_sistema()
